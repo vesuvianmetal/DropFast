bfbf74a79fab535a30ed3264be3431e3d86d55b0
Samuel Salgado Gonzalez <vesuvianmetal@hotmail.com>
2017-06-13 10:42:58 -0700

    button look

--- DropFast/DropFast/Form1.Designer.cs
+++ DropFast/DropFast/Form1.Designer.cs
@@ -28,16 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtlook = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.txtsearch = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtlook
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 41);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(184, 22);
-            this.textBox1.TabIndex = 0;
+            this.txtlook.Location = new System.Drawing.Point(12, 41);
+            this.txtlook.Name = "txtlook";
+            this.txtlook.Size = new System.Drawing.Size(184, 22);
+            this.txtlook.TabIndex = 0;
             // 
             // label1
             // 
@@ -48,13 +50,32 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "What Are You Looking For?";
             // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(216, 18);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // txtsearch
+            // 
+            this.txtsearch.Location = new System.Drawing.Point(62, 69);
+            this.txtsearch.Name = "txtsearch";
+            this.txtsearch.Size = new System.Drawing.Size(75, 23);
+            this.txtsearch.TabIndex = 3;
+            this.txtsearch.Text = "Search";
+            this.txtsearch.UseVisualStyleBackColor = true;
+            // 
             // Dropfast
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(727, 490);
+            this.Controls.Add(this.txtsearch);
+            this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtlook);
             this.Name = "Dropfast";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DropFast";
@@ -65,8 +86,10 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtlook;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button txtsearch;
     }
 }
 
